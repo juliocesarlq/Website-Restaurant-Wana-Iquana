3ede295855122d629bfa0980619e360b0938d091
Julio Cesar Lopez <lopezjuliocesar1994@gmail.com>
2022-12-01 10:34:29 -0800

    updated content, changed carousel

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 //animation for left screen side entry 
+//was attempting to create right and left side entries but when I used anchors google chrome has massive bug
 const observerLeft = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -30,5 +31,40 @@ const observerRight = new IntersectionObserver((entries) => {
 const hiddenElementsRight = document.querySelectorAll('.hidden-right');
 hiddenElementsRight.forEach((el) => observerRight.observe(el));
 
+let foodItem = [
+    {
+        id: 1,
+        name: "baja burrito",
+        category: "burrito",
+        description: "Steak or chicken sautéed, peppers, onions, zucchini, and tomato sauce.",
+        price: 10.49,
+    },
 
+    {
+        id: 2,
+        name: "baja taco",
+        category: "taco",
+        description: "Steak or chicken sautéed, peppers, onions, zucchini, and tomato sauce.",
+        price: 8.49,
+    }
+];
+
+//generating items
+
+
+
+function createCards() {
+    let shop = document.getElementById("shop")
+    for (let i = 0; i < foodItem.length; i++) { 
+
+        let heading = document.createElement("h1");
+        heading.setAttribute("id","food-id-${foodItem[i].id}");
+        shop.appendChild(heading);
+
+        console.log(foodItem[i].id);
+      }
+
+}
+
+createCards();
 
